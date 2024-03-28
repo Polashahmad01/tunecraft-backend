44a5fa67936e66cf6960dbb144795897be4d6788
Polashahmad01 <dev.polashahmad@gmail.com>
2024-03-28 08:56:38 +0600

    Refactor email validation messages in authValidation

--- utils/authValidation.js
+++ utils/authValidation.js
@@ -4,7 +4,7 @@ const validateRegisterRoute = () => {
   return [
     body("firstName", "First name cannot be empty.").trim().notEmpty(),
     body("lastName", 'Last name cannot be empty.').trim().notEmpty(),
-    body("email", "Invalid email address.").trim().normalizeEmail().isEmail(),
+    body('email').trim().isEmail().withMessage("Invalid email address."),
     body("password", "Password cannot be empty.").trim().notEmpty(),
     body("password", "Password should be alpha-numeric.").isAlphanumeric(),
     body("password", "Password should be at least 6 characters long.").isLength({ min: 6 })
@@ -13,7 +13,7 @@ const validateRegisterRoute = () => {
 
 const validateLoginRoute = () => {
   return [
-    body("email").trim().notEmpty().withMessage("Email cannot be empty.").isEmail().withMessage("Not a valid email address.").normalizeEmail().withMessage("Invalid email address."),
+    body('email').trim().isEmail().withMessage("Invalid email address."),
     body("password").trim().notEmpty().withMessage("Password cannot be empty.").isAlphanumeric().withMessage("Password should be alpha-numeric.").isLength({ min: 6 }).withMessage("Password should be at list 6 characters long.")
   ]
 }
@@ -28,7 +28,7 @@ const validateLoginSocialRoute = () => {
 
 const validateForgotPasswordRoute = () => {
   return [
-    body('email').trim().notEmpty().withMessage("Email cannot be empty.").isEmail().withMessage("Not a valid email address.").normalizeEmail().withMessage("Invalid email address.")
+    body('email').trim().isEmail().withMessage("Invalid email address.")
   ]
 }
 
